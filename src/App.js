9657e934d5e1e87926269be2ecea571b83654974
danielmoralesp <daniel_moralesp@hotmail.com>
2017-08-31 13:26:57 -0500

    Very basci components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import Welcome from './Welcome'
 import './App.css';
 
+
+
 class App extends Component {
+  constructor(){
+    super();
+    this.changeTitle = this.changeTitle.bind(this)
+
+    this.state = {
+      title: "Hola mundo desde estado"
+    }
+  }
+
   render() {
     const names = ["Daniel", "Angelica", "Emiliano"]
     return (
       <div>
-        <h1>Hola Mundo </h1>
+        <Welcome name="Daniel" />
+        <Welcome name="Angelica" />
+          {names.map (name =>
+            <Welcome name={name} />
+          )}
         <p>Parrafo</p>
+        <h2>{this.state.title}</h2>
+        <button onClick={this.changeTitle}>Cambie el titulo</button>
         <ul>
           {names.map (name =>
             <li>{name}</li>
@@ -15,6 +33,12 @@ class App extends Component {
         </ul>
       </div>
     );
+  }
+
+  changeTitle(){
+    this.setState({
+      title: "Nuevo titulo"
+    });
   }
 }
 

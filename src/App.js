c055afdff88bee9fb453905d95a655ceef8d81d1
danielmoralesp <daniel_moralesp@hotmail.com>
2017-08-31 12:56:40 -0500

    Very basic JSX

--- src/App.js
+++ src/App.js
@@ -3,8 +3,17 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const names = ["Daniel", "Angelica", "Emiliano"]
     return (
-      <h1>Hola Mundo </h1>
+      <div>
+        <h1>Hola Mundo </h1>
+        <p>Parrafo</p>
+        <ul>
+          {names.map (name =>
+            <li>{name}</li>
+          )}
+        </ul>
+      </div>
     );
   }
 }
